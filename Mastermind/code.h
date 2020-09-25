01b21c31d1a4f1fc58733af595cc05b92f559e72
alexnegulescu <71865036+alexnegulescu@users.noreply.github.com>
2020-09-25 00:45:55 -0400

    Update code.h

--- Mastermind/code.h
+++ Mastermind/code.h
@@ -9,34 +9,34 @@ using namespace std;
 
 // Declaration
 
-class Code 
+class Code
 {
-	private:
-		int size, range;
-		vector<int> secretCode;
+private:
+	int size, range;
+	vector<int> secretCode, secretCopy, guessCopy;
 
-	public:
-		// Constructor
-		Code(int n, int m)
-		{
-			size = n;  
-			range = m;
-		}
+public:
+	// Constructor
+	Code(int n, int m)
+	{
+		size = n;
+		range = m;
+	}
 
-		// Public Member Functions
-		vector<int> create();
-		int checkCorrect(vector<int> guess);
-		int checkIncorrect(vector<int> guess);
+	// Public Member Functions
+	vector<int> create();
+	int checkCorrect(vector<int> guess);
+	int checkIncorrect(vector<int> guess);
 
 };
 
 // Implementation
 
-vector<int> Code::create() 
+vector<int> Code::create()
 {
 	srand(time(NULL));
 	int randomDigit;
-	for (int i = 0; i < size; i++) 
+	for (int i = 0; i < size; i++)
 	{
 		randomDigit = rand() % range;
 		secretCode.push_back(randomDigit);
@@ -46,33 +46,39 @@ vector<int> Code::create()
 
 int Code::checkCorrect(vector<int> guess)
 {
+	secretCopy = secretCode;
+	guessCopy = guess;
 	int correct = 0;
-	for (int x = 0; x < guess.size(); x++)
+	for (int x = 0; x < guessCopy.size(); x++)
 	{
-		if (guess[x] == secretCode[x])
+		if (guessCopy[x] == secretCopy[x])
 		{
 			correct++;
+			guessCopy[x] = range + 1;
+			secretCopy[x] = range + 2;
 		}
 	}
 	return correct;
 }
 
 
-int Code::checkIncorrect(vector<int> guess) 
+int Code::checkIncorrect(vector<int> guess)
 {
 	int correct = 0;
 
-	for (int x = 0; x < guess.size(); x++)
+	for (int x = 0; x < guessCopy.size(); x++)
 	{
-		for (int y = 0; y < secretCode.size(); y++)
+		for (int y = 0; y < secretCopy.size(); y++)
 		{
-			if (guess[x] == secretCode[y] && x == y)
+			if (guessCopy[x] == secretCopy[y] && x == y)
 			{
 				break;
 			}
-			else if (guess[x] == secretCode[y] && x != y)
+			else if (guessCopy[x] == secretCopy[y] && x != y)
 			{
 				correct++;
+				guessCopy[x] = range + 1;
+				secretCopy[y] = range + 2;
 				break;
 			}
 		}
